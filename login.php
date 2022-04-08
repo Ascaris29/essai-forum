39898e2c2101a9ff9176faae95d0a7ec542723cc
Ascaris29 <98960584+Ascaris29@users.noreply.github.com>
2022-04-09 01:29:55 +0200

    Add files via upload

---
+++ login.php
@@ -0,0 +1,28 @@
+<?php require('actions/users/loginAction.php'); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<?php require ('includes/head.php'); ?>
+<body>
+<br><br>
+    <form class="container" method="POST">
+
+        <?php if(isset($errorMsg)){ echo '<p>'.$errorMsg.'</p>'; } ?>
+
+        <div class="mb-3">
+            <label for="pseudo" class="form-label">Pseudo</label>
+            <input type="text" class="form-control" name="pseudo">
+        </div>
+        
+        <div class="mb-3">
+            <label for="password" class="form-label">Mot de passe </label>
+            <input type="password" class="form-control" name="password">
+        </div>
+        <button type="submit" class="btn btn-primary" name="validate">Se connecter </button>
+        
+        <br><br>
+        <a href="signup.php"><p> Je n'ai pas de compte, je m'inscris </p></a>
+        
+   </form>
+</body>
+</html>
---
+++ my-questions.php
@@ -0,0 +1,45 @@
+<?php require('actions/questions/myQuestionsAction.php');
+      require('actions/users/securityAction.php');
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<?php include ('includes/head.php') ?>
+<body>
+    <?php include ('includes/navbar.php') ?>
+
+    <br></br>
+    <div class="container">
+    <?php
+            while($question = $getAllMyQuestions->fetch()){
+    ?>
+                
+                <div class="card">
+                    <h5 class="card-header">
+                      <?php echo $question['title'];?>
+                    </h5>
+                    <div class="card-body">
+                        <p class="card-text">
+                            <?php echo $question['description']; ?>
+                        </p>
+                        <a href="#" class="btn btn-primary">Acceder à la question </a>
+                        <a href="#" class="btn btn-warning">Modifier la question </a>
+                        
+                    </div>
+                    </div>
+                    <br></br>
+                <?php
+
+            }
+            
+            ?>
+
+        
+    
+    
+
+
+</div>
+</body>
+</html>
