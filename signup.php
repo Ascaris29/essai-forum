4e6698e57a34a84864ddaefe71eb61b2f69b69fc
solenelema2009€hotmail.com <solenelema20090505@hotmail.com>
2022-04-17 18:26:53 +0200

    ajout css

--- signup.php
+++ signup.php
@@ -1,36 +1,52 @@
 <?php require('actions/users/signupAction.php'); ?>
 <!DOCTYPE html>
 <html lang="en">
-<?php require ('includes/head.php'); ?>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--code css bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <!-- css -->
+    <link rel="stylesheet" href="assets/signup.css">
+    <!--code javascript bootstrap -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <!-- javascript -->
+    
+    <title>Forum</title>
+</head>
+
 <body>
-    <br><br>
-    <form class="container" method="POST">
+    
+    <h1> La nouvelle </h1>
+            
+            <form class="container" method="POST">
 
-        <?php if(isset($errorMsg)){ echo '<p>'.$errorMsg.'</p>'; } ?>
+                <?php if(isset($errorMsg)){ echo '<p>'.$errorMsg.'</p>'; } ?>
 
-        <div class="mb-3">
-            <label for="pseudo" class="form-label">Pseudo</label>
-            <input type="text" class="form-control" name="pseudo">
-        </div>
-        <div class="mb-3">
-            <label for="lastname" class="form-label">Nom</label>
-            <input type="text" class="form-control" name="lastname">
-        </div>
-        <div class="mb-3">
-            <label for="firstname" class="form-label">Prénom</label>
-            <input type="text" class="form-control" name="firstname">
-        </div>
-        <div class="mb-3">
-            <label for="password" class="form-label">Mot de passe </label>
-            <input type="password" class="form-control" name="password">
-        </div>
-        <button type="submit" class="btn btn-primary" name="validate">S'inscrire</button>
+                <div class="mb-3">
+                    <label for="pseudo" class="form-label">Pseudo</label>
+                    <input type="text" class="form-control" name="pseudo">
+                </div>
+                <div class="mb-3">
+                    <label for="lastname" class="form-label">Nom</label>
+                    <input type="text" class="form-control" name="lastname">
+                </div>
+                <div class="mb-3">
+                    <label for="firstname" class="form-label">Prénom</label>
+                    <input type="text" class="form-control" name="firstname">
+                </div>
+                <div class="mb-3">
+                    <label for="password" class="form-label">Mot de passe </label>
+                    <input type="password" class="form-control" name="password">
+                </div>
+                <button type="submit" class="btn btn-primary" name="validate">S'inscrire</button>
 
-        <br><br>
-        <a href="login.php"><p> J'ai déjà un compte, je me connecte </p></a>
-        
-        
-   </form>
+                <br><br>
+                <p class="dir-signiup"><a href="login.php"> J'ai déjà un compte, je me connecte </a></p>
+                
+                
+        </form>
 
 </body>
 </html>
